4a910165743f4a08821354933f985616f166f4e1
Ashraful Islam <ashrafule44@gmail.com>
2024-12-08 12:54:39 +0600

    Floyd-Warshall algorithm

--- warshall.cpp
+++ warshall.cpp
@@ -10,39 +10,26 @@ int main()
     cin >> V;
 
     vector<vector<int>> graph(V, vector<int>(V));
-    cout << "Enter the adjacency matrix values (use a large value like 999 for INF):" << endl;
+    cout << "Enter the adjacency matrix values (use 999 for INF):" << endl;
     for (int i = 0; i < V; i++)
-    {
         for (int j = 0; j < V; j++)
         {
             cin >> graph[i][j];
             if (graph[i][j] == 999)
                 graph[i][j] = INT_MAX;
         }
-    }
 
     for (int k = 0; k < V; k++)
-    {
         for (int i = 0; i < V; i++)
-        {
             for (int j = 0; j < V; j++)
-            {
                 if (graph[i][k] != INT_MAX && graph[k][j] != INT_MAX)
                     graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
-            }
-        }
-    }
 
     cout << "\nShortest path matrix after Floyd-Warshall:" << endl;
-    for (int i = 0; i < V; i++)
+    for (const auto &row : graph)
     {
-        for (int j = 0; j < V; j++)
-        {
-            if (graph[i][j] == INT_MAX)
-                cout << "INF ";
-            else
-                cout << graph[i][j] << " ";
-        }
+        for (int dist : row)
+            cout << (dist == INT_MAX ? "INF" : to_string(dist)) << " ";
         cout << endl;
     }
 
